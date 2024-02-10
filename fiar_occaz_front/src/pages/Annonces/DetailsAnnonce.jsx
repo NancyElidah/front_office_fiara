1779eb91978c0afcad669bb312ed199d8a5281cc
NancyElidah <nancyandriamahanintsoa@gmail.com>
2024-02-10 16:20:03 +0100

    other

--- fiar_occaz_front/src/pages/Annonces/DetailsAnnonce.jsx
+++ fiar_occaz_front/src/pages/Annonces/DetailsAnnonce.jsx
@@ -4,7 +4,6 @@ import Footer from "../Footer";
 import sary from "../assets/vendors/images/v1.jpg";	
 import { Link } from "react-router-dom";
 import AnnonceServiceInstance from "../service/AnnonceService";
-import { useEffect } from "react";
 
 class DetailsAnnonce extends Component{
 	constructor(props){
