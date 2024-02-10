0eb8c9ea94c87d7cd4166a327b1ddbcfb0147b4c
NancyElidah <nancyandriamahanintsoa@gmail.com>
2024-02-10 16:59:44 +0100

    nom user

--- fiar_occaz_front/src/pages/Annonces/DetailsAnnonce.jsx
+++ fiar_occaz_front/src/pages/Annonces/DetailsAnnonce.jsx
@@ -33,7 +33,7 @@ class DetailsAnnonce extends Component{
 
         AnnonceServiceInstance.getOneAnnonce(this.state.id).then((res) => {
             this.setState({ 
-                user :res.data.utilisateur.nom,
+                user :res.data.utilisateur,
                 description : res.data.description ,
                 prix:res.data.voiture.prix,
                 type:res.data.voiture.type.nom,
